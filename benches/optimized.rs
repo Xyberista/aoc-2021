0dd3a959bce24a563e7ebb6292d376a2ef6953fc
xyberista <50119873+Xyberista@users.noreply.github.com>
2022-08-31 14:22:49 -0500

    fairer benchmarking

--- benches/optimized.rs
+++ benches/optimized.rs
@@ -14,6 +14,18 @@ pub fn day_1(c: &mut Criterion) {
     group.finish();
 }
 
+pub fn day_2(c: &mut Criterion) {
+    let mut group = c.benchmark_group("day_2 optimized");
+    let instructions = days_optimized::day_2::get_instructions();
+    group.bench_function("part 1", |b| {
+        b.iter(|| days_optimized::day_2::part_1(black_box(&instructions)))
+    });
+    group.bench_function("part 2", |b| {
+        b.iter(|| days_optimized::day_2::part_2(black_box(&instructions)))
+    });
+    group.finish();
+}
+
 pub fn day_15(c: &mut Criterion) {
     let mut group = c.benchmark_group("day_15 optimized");
     let board = days_optimized::day_15::get_board();
@@ -26,11 +38,10 @@ pub fn day_15(c: &mut Criterion) {
     group.finish();
 }
 
-criterion_group!(alpha, day_1);
+criterion_group!(alpha, day_1, day_2);
 // criterion_group!(beta, day_6);
 criterion_group!(
-    gamma, 
-    // day_11,
+    gamma, // day_11,
     // day_12,
     day_15
 );
--- src/days/day_1.rs
+++ src/days/day_1.rs
@@ -1,17 +1,19 @@
 use super::super::utils::*;
 
-pub fn input() -> String {
+pub fn input() -> Vec<i32> {
     get_input(1)
+        .lines()
+        .map(|c| c.parse::<i32>().unwrap())
+        .collect()
 }
 
 pub fn day_1() {
-    let input = get_input(1);
+    let input = input();
     println!("{}", part_one(&input));
     println!("{}", part_two(&input));
 }
 
-pub fn part_one(input: &str) -> i32 {
-    let lines: Vec<i32> = input.lines().map(|c| c.parse::<i32>().unwrap()).collect();
+pub fn part_one(lines: &[i32]) -> i32 {
     let mut t = 0;
     for w in lines.windows(2) {
         if w[0] < w[1] {
@@ -20,8 +22,7 @@ pub fn part_one(input: &str) -> i32 {
     }
     t
 }
-pub fn part_two(input: &str) -> i32 {
-    let lines: Vec<i32> = input.lines().map(|c| c.parse::<i32>().unwrap()).collect();
+pub fn part_two(lines: &[i32]) -> i32 {
     let mut t = 0;
     let mut windows = lines.windows(3);
     let mut p = windows.next().unwrap().iter().sum::<i32>();
@@ -41,13 +42,13 @@ mod test {
 
     #[test]
     fn test_one() {
-        let input = get_input(1);
+        let input = input();
         assert_eq!(part_one(&input), 1228);
     }
 
     #[test]
     fn test_two() {
-        let input = get_input(1);
+        let input = input();
         // change one -> two
         assert_eq!(part_two(&input), 1257);
     }
